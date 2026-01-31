744901de64c0c7496595020227d7f98d06b2d4c6
Evy-Durden <patrick.rahn.cfh@gmail.com>
2026-01-31 18:22:23 +0100

    Refactoring identation

--- src/linux_analyzer/cli/menu.py
+++ src/linux_analyzer/cli/menu.py
@@ -9,8 +9,8 @@ def show_menu():
 	flags = prompt_choice()
 
 	if flags["mode"] == "static":
-			info("You have chosen the static mode.")
+		info("You have chosen the static mode.")
 	elif flags["mode"] == "behavioral":
-			info("You have chosen the behavioral mode.")
+		info("You have chosen the behavioral mode.")
 	elif flags["mode"] == "full":
-			info("You have chosen the full mode.")
+		info("You have chosen the full mode.")
