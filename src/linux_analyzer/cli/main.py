7559dae9220109fe4748d573022de2ba14f48486
Evy-Durden <patrick.rahn.cfh@gmail.com>
2026-01-31 18:18:47 +0100

    Refactoring identation

--- src/linux_analyzer/cli/main.py
+++ src/linux_analyzer/cli/main.py
@@ -6,14 +6,14 @@ app = typer.Typer()
 
 @app.command()
 def main():
-		"""
-		The @app.command() decorator turns a function into a CLI command.
-		Main simple calls into menu.py and does not implement any menu logic itself.
-		The reason is separation of concern for testability, readability and maintainability.
-		"""
-		show_menu()
+	"""
+	The @app.command() decorator turns a function into a CLI command.
+	Main simple calls into menu.py and does not implement any menu logic itself.
+	The reason is separation of concern for testability, readability and maintainability.
+	"""
+	show_menu()
 	
 # ensure that code is only run when script is executed and not when it is imported
 if __name__ == "__main__":
-		app()
+	app()
 
--- src/linux_analyzer/cli/menu.py
+++ src/linux_analyzer/cli/menu.py
@@ -3,14 +3,14 @@ from .output import *
 
 
 def show_menu():
-		header("Welcome to the Linux Analyzer Tool")
-	
-		# User input
-		flags = prompt_choice()
-	
-		if flags["mode"] == "static":
-				info("You have chosen the static mode.")
-		elif flags["mode"] == "behavioral":
-				info("You have chosen the behavioral mode.")
-		elif flags["mode"] == "full":
-				info("You have chosen the full mode.")
+	header("Welcome to the Linux Analyzer Tool")
+
+	# User input
+	flags = prompt_choice()
+
+	if flags["mode"] == "static":
+			info("You have chosen the static mode.")
+	elif flags["mode"] == "behavioral":
+			info("You have chosen the behavioral mode.")
+	elif flags["mode"] == "full":
+			info("You have chosen the full mode.")
